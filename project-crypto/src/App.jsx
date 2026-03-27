93078eb60f2e397a0ec9b5ec1d3f775233418636
Vitor Zuketta de Borba <kunzuketta@gmail.com>
2026-03-27 19:04:17 -0300

    deploy na vercel e redirecionamento de rotas.

--- project-crypto/src/App.jsx
+++ project-crypto/src/App.jsx
@@ -1,10 +1,14 @@
 import Dashboard from './pages/Dashboard/Dashboard';
 import Transacao from './pages/Transacao/Transacao';
 import Suporte from './pages/Suporte/Suporte';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/central" replace />,
+  },
   {
     path: "/central",
     element: <Dashboard />,
